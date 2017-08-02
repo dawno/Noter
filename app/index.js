6e471f750be320091eaf8ccfcaba14a8fcbf1418
dawno <utkarshsinghcs@gmail.com>
2017-08-02 11:06:12 +0530

    functions created

---
+++ app/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import AppContainer from './AppContainer';
+import reducer from './reducer';
+const store = createStore(reducer);
+export default class Index extends components {
+
+    render() { // eslint-disable-line
+        return (
+            <Provider store={store} >
+                <AppContainer />
+            </Provider>
+        );
+    }
+}
